81fae1f3a7f548431f41d1c0754a979a25b37a75
Shota Iwanaga <mwam090827@icloud.com>
2023-06-17 09:15:02 +0900

    CCCCCCを追加しました

--- GitTest01/ViewController.swift
+++ GitTest01/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("AAAAAA")
         print("BBBBBB")
+        print("CCCCCCFromMain")
     }
 
 
